f5dbd8f7ce69017f12ac2d1e0f62921167177131
sumaautomationLK <sumanga0000@gmail.com>
2024-11-13 13:24:20 +0530

    Add websocket sever

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app);
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({server:server});
+var dataincomming;
+wss.on('connection', function connection(ws) {
+    //**************************************** */
+  console.log('A new client Connected!');
+  ws.send('Welcome New Client!');
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    ws.send("you pass value is:"+message);
+    dataincomming=message;
+   // Share message all connected client
+   wss.clients.forEach(function each(client) {
+    if (client !== ws && client.readyState === WebSocket.OPEN) {
+      client.send(message.toString());
+    }
+  });    
+
+
+
+    
+
+     });
+   
+    //****************************** */
+ 
+});
+
+app.get('/', (req, res) => res.send(
+  `incomming data:${dataincomming}`))
+
+server.listen(3000, () => console.log(`Lisening on port :3000`))       
